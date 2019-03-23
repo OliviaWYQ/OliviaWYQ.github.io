8be2cf8b1eb4fbc4d05809195435ca0ab42c5705
Olivia Wang <oliviawang@OliviadeMBP.fios-router.home>
2019-03-23 02:23:10 -0400

    update content

--- blog_v2/src/Components/Portfolio.js
+++ blog_v2/src/Components/Portfolio.js
@@ -10,7 +10,7 @@ class Portfolio extends Component {
            <div className="item-wrap">
             <a href={projects.url} title={projects.title}>
                <img alt={projects.title} src={projectImage} />
-               <div className="overlay">
+               <div className="overlay" >
                   <div className="portfolio-item-meta">
                  <h5>{projects.title}</h5>
                      <p>{projects.category}</p>
@@ -30,7 +30,7 @@ class Portfolio extends Component {
 
          <div className="twelve columns collapsed">
 
-            <h1>Check Out Some of My Works.</h1>
+            <h1>Check Out Some of My Academic Projects</h1>
 
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
--- blog_v2/src/Components/Resume.js
+++ blog_v2/src/Components/Resume.js
@@ -13,7 +13,7 @@ class Resume extends Component {
       var work = this.props.data.work.map(function(work){
         return <div key={work.company}><h3>{work.company}</h3>
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p>{work.description}</p>
+            <p > {work.description_1} <br /> {work.description_2}</p>
         </div>
       })
       var skills = this.props.data.skills.map(function(skills){
@@ -43,13 +43,13 @@ class Resume extends Component {
       <div className="row work">
 
          <div className="three columns header-col">
-            <h1><span>Work</span></h1>
+            <h1><span>Experience</span></h1>
          </div>
 
          <div className="nine columns main-col">
           {work}
         </div>
-    </div>
+      </div>
 
 
 
